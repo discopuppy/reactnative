91295711fd1cef7e8749fb90091748bfe5b93e9d
Zachary Moss <zachary.moss@ymail.com>
2017-03-08 13:56:19 -0600

    Added Card component and CardSection component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Card from './Card.js';
+import CardSection from './CardSection.js';
+
+const AlbumDetail = (props) => {
+    return (
+        <Card>
+            <CardSection>
+                <View></View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
+};
+
+export default AlbumDetail;
--- src/components/header.js
+++ src/components/header.js
@@ -22,7 +22,7 @@ const styles = {
         paddingTop: 15,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2
+        shadowOpacity: 0.2,
         elevation: 2,
         position: 'relative'
     },
