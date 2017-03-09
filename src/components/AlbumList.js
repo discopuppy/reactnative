31c7a0d533745ad9b771af037c296f32a4bb3276
Zachary Moss <zachary.moss@ymail.com>
2017-03-08 18:41:26 -0600

    Added Album styling and Button

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -17,10 +17,12 @@ class AlbumList extends Component {
     }
 
     render() {
+        console.log(this.state);
+
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
